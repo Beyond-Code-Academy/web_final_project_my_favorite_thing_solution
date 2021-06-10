51912e84b2b06971401fb6dc25f1a6fa9ea9e916
teerapatBeyond <76890317+teerapat-beyonndcode-academy@users.noreply.github.com>
2021-06-10 08:22:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from random import choice
+
+web_site = Flask(__name__)
+
+
+@web_site.route('/')
+def index():
+	return render_template('index.html')
+
+
+@web_site.route('/Consoles')
+def Consoles():
+	return render_template('Consoles.html')
+
+@web_site.route('/Games')
+def Games():
+	return render_template('Games.html')
+
+@web_site.route('/Contact')
+def Contact():
+	return render_template('Contact.html')
+
+
+
+web_site.run(host='0.0.0.0', port=8080)
